f651902ebcc4662b59c7848cb331d441f082e2fe
rowlul <96617295+rowlul@users.noreply.github.com>
2022-05-05 22:14:29 +0300

    Use Mutex to run single instance

--- DevServer/Program.cs
+++ DevServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
@@ -11,9 +12,29 @@ namespace DevServer
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
+        
+        private const int TimeoutSeconds = 3;
+        
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            var mutex = new Mutex(false, typeof(Program).FullName);
+
+            try
+            {
+                if (!mutex.WaitOne(TimeSpan.FromSeconds(TimeoutSeconds), true))
+                {
+                    return;
+                }
+                
+                BuildAvaloniaApp()
+                    .StartWithClassicDesktopLifetime(args);
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
