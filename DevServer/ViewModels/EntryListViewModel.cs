4d2aeb1f716d430370a1e59636926adf34a9ac50
rowlul <96617295+rowlul@users.noreply.github.com>
2022-06-30 21:30:03 +0300

    Implement bare-bones of EntryViewModel and EntryListViewModel

--- DevServer/ViewModels/EntryListViewModel.cs
+++ DevServer/ViewModels/EntryListViewModel.cs
@@ -1,6 +1,23 @@
+using System.Collections.ObjectModel;
+
+using ReactiveUI;
+
 namespace DevServer.ViewModels;
 
 public class EntryListViewModel : ViewModelBase
 {
-    
+    private ObservableCollection<EntryViewModel> _entries;
+    private EntryViewModel _selectedEntry;
+
+    public ObservableCollection<EntryViewModel> Entries
+    {
+        get => _entries;
+        set => this.RaiseAndSetIfChanged(ref _entries, value);
+    }
+
+    public EntryViewModel SelectedEntry
+    {
+        get => _selectedEntry;
+        set => this.RaiseAndSetIfChanged(ref _selectedEntry, value);
+    }
 }
---
+++ DevServer/ViewModels/EntryViewModel.cs
@@ -0,0 +1,31 @@
+using Avalonia.Media.Imaging;
+
+using DevServer.Models;
+
+using ReactiveUI;
+
+namespace DevServer.ViewModels;
+
+public class EntryViewModel : ViewModelBase
+{
+    private readonly Entry _entry;
+    
+    private Bitmap? _logo;
+
+    public EntryViewModel(Entry entry)
+    {
+        _entry = entry;
+    }
+
+    public string Name => _entry.Name;
+    public string? Description => _entry.Description;
+    
+    public Bitmap? Logo
+    {
+        get => _logo;
+        private set => this.RaiseAndSetIfChanged(ref _logo, value);
+    }
+    
+    // TODO: load logo async method
+    // TODO: do business in a seperate service
+}
