f1bc73d9225577446a52d68476f6ff2ec6a57f67
rowlul <96617295+rowlul@users.noreply.github.com>
2022-06-30 14:40:28 +0300

    Create play command and observe input in text box

--- DevServer/ViewModels/ToolBarPanelViewModel.cs
+++ DevServer/ViewModels/ToolBarPanelViewModel.cs
@@ -1,9 +1,34 @@
+using System;
+using System.Reactive;
+using System.Threading.Tasks;
+
+using ReactiveUI;
+
 namespace DevServer.ViewModels;
 
 public class ToolBarPanelViewModel : ViewModelBase
 {
+    private string _serverAddress;
+    
     public ToolBarPanelViewModel()
     {
-        
+        IObservable<bool> isNotEmpty = this.WhenAnyValue(
+            x => x.ServerAddress,
+            x => !string.IsNullOrWhiteSpace(x));
+
+        PlayCommand = ReactiveCommand.CreateFromTask(Play, isNotEmpty);
+    }
+
+    public string ServerAddress
+    {
+        get => _serverAddress;
+        set => this.RaiseAndSetIfChanged(ref _serverAddress, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> PlayCommand { get; }
+
+    private Task Play()
+    {
+        return Task.CompletedTask;
     }
 }
