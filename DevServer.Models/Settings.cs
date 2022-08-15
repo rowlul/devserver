d83e57101143adb7d403f21fa7f921006dfaea15
rowlul <96617295+rowlul@users.noreply.github.com>
2022-08-15 17:41:33 +0300

    Use wine defaults if on linux

--- DevServer.Models/Settings.cs
+++ DevServer.Models/Settings.cs
@@ -2,18 +2,18 @@ namespace DevServer.Models;
 
 public class Settings
 {
-    private static string GetPath()
-    {
-        if (OperatingSystem.IsWindows())
-        {
-            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-                                "osu!",
-                                "osu!.exe");
-        }
+    private static string GetPath() =>
+        OperatingSystem.IsWindows()
+            ? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                           "osu!",
+                           "osu!.exe")
+            : string.Empty;
 
-        return string.Empty;
-    }
+    private static WineStartInfo? GetWineSettings() =>
+        OperatingSystem.IsLinux()
+            ? new WineStartInfo(Path: "/usr/bin/wine", Prefix: "~/.wineprefix", Arch: WineArch.Win32, null)
+            : null;
 
     public string OsuExePath { get; set; } = GetPath();
-    public WineStartInfo? WineSettings { get; set; }
+    public WineStartInfo? WineSettings { get; set; } = GetWineSettings();
 }
