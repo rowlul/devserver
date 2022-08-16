da03bc82242935b3f056edcd4432e3dc4a44ceee
rowlul <96617295+rowlul@users.noreply.github.com>
2022-08-16 16:09:00 +0300

    Make default wineprefix path absolute
    Wine required path to be absolute

--- DevServer.Models/Settings.cs
+++ DevServer.Models/Settings.cs
@@ -15,7 +15,8 @@ public class Settings
     private static WineStartInfo? GetWineSettings() =>
         OperatingSystem.IsLinux()
             ? new WineStartInfo(Path: "/usr/bin/wine",
-                                Prefix: "~/.wineprefix",
+                                Prefix:
+                                $"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}/.wineprefix",
                                 Arch: WineArch.Win32,
                                 new Dictionary<string, string>())
             : null;
