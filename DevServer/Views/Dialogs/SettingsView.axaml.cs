642f324886c7408025cf9302308d14e89c2703ee
rowlul <96617295+rowlul@users.noreply.github.com>
2022-08-24 20:10:20 +0300

    Create SettingView dialog

---
+++ DevServer/Views/Dialogs/SettingsView.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DevServer.Views.Dialogs;
+
+public partial class SettingsView : Window
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
