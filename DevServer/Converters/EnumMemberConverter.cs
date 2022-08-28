fc30befa859990ca3c783b7a4cb1f8c2241d6155
rowlul <96617295+rowlul@users.noreply.github.com>
2022-08-28 15:54:29 +0300

    Implement EnumMemberConverter

---
+++ DevServer/Converters/EnumMemberConverter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+using Avalonia.Data.Converters;
+
+namespace DevServer.Converters;
+
+public class EnumMemberConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var @enum = (Enum)value;
+        var fieldInfo = value.GetType().GetField(@enum.ToString())!;
+        var attributes = fieldInfo.GetCustomAttributes(inherit: false);
+
+        if (attributes.Length == 0)
+        {
+            return Enum.GetName(@enum.GetType(), @enum);
+        }
+
+        string? enumValue = null;
+        foreach (var attribute in attributes)
+        {
+            if (attribute is EnumMemberAttribute enumMemberAttribute)
+            {
+                enumValue = enumMemberAttribute.Value;
+            }
+        }
+
+        return enumValue ?? Enum.GetName(@enum.GetType(), @enum);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return string.Empty;
+    }
+}
