24cd38260a6658c622d2fe09b9e4e957fb07600b
rowlul <96617295+rowlul@users.noreply.github.com>
2022-09-19 16:02:37 +0300

    Create ShowEntryEditViewModel extension method

--- DevServer/Extensions/DialogServiceExtensions.cs
+++ DevServer/Extensions/DialogServiceExtensions.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 
 using CommunityToolkit.Mvvm.DependencyInjection;
 
+using DevServer.Models;
 using DevServer.ViewModels;
 using DevServer.ViewModels.Dialogs;
 
@@ -30,5 +31,13 @@ public static class DialogServiceExtensions
         var owner = Ioc.Default.GetRequiredService<MainWindowViewModel>();
         var modal = Ioc.Default.GetRequiredService<SettingsViewModel>();
         return service.ShowDialogAsync(owner, modal);
+    }
+
+    public static async Task<Entry?> ShowEntryEditViewModel(this IDialogService service, Entry? entry = null)
+    {
+        var owner = Ioc.Default.GetRequiredService<MainWindowViewModel>();
+        var modal = new EntryEditViewModel(entry);
+        var result = await service.ShowDialogAsync(owner, modal);
+        return result == true ? modal.Entry : null;
     }
 }
