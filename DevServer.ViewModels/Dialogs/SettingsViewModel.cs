80399755db2144faba4f0fc04dca9c22ac0ffd34
rowlul <96617295+rowlul@users.noreply.github.com>
2022-10-28 17:42:48 +0300

    Remove reload settings button

--- DevServer.ViewModels/Dialogs/SettingsViewModel.cs
+++ DevServer.ViewModels/Dialogs/SettingsViewModel.cs
@@ -91,25 +91,6 @@ public partial class SettingsViewModel : DialogViewModelBase
     }
 
     [RelayCommand]
-    private async Task Reload()
-    {
-        var result = await _dialogService.ShowMessageBox(
-            this,
-            "Are you sure to reload settings from file?",
-            "Any pending changes will be lost.",
-            MessageBoxIcon.Question,
-            MessageBoxButtons.OkCancel);
-
-        if (result is false)
-        {
-            return;
-        }
-
-        await _configurationManager.LoadAsync();
-        Load();
-    }
-
-    [RelayCommand]
     private async Task Reset()
     {
         var result = await _dialogService.ShowMessageBox(
